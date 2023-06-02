cbbc7c5cfdc643bfff65d40626a75fe4292a3c0f
Loic Leray <hi@loicleray.com>
2023-06-02 17:45:08 +0200

    Updading of the main game.

--- scripts/main.js
+++ scripts/main.js
@@ -37,8 +37,10 @@ function Player(name, symbol) {
 // Game Logic
 function GameLogic(Player1, Player2) {
     
-    let isGameOver = false;
-    let gameWinner = null
+    let _isGameOver = false;
+    let _gameWinner = null;
+    let _winningCombo = null;
+
     const _players = [Player1, Player2];
     let _currentPlayer = _players[0];
 
@@ -64,120 +66,157 @@ function GameLogic(Player1, Player2) {
             console.log(`Space \'${index}\' is already taken by \'${player.getName()}\'.`);
         }};
 
-    // Check for winning combinations TK
-    const checkGameStatus = () => {
-            let usedFields = {
-                            'Player1_positions': [],
-                            'Player2_positions': [],
-                            'Null_positions': [],
-                        };
-
-            const winningPositions = [
-                                        [0, 1, 2],
-                                        [3, 4, 5],
-                                        [6, 7, 8],
-                                        [0, 3, 6],
-                                        [1, 4, 7],
-                                        [2, 5, 8],
-                                        [0, 4, 8],
-                                        [2, 4, 6],
-                                    ];
+    const _getBoardPositions = () => {
+        let usedFields = {
+            'Player1_positions': [],
+            'Player2_positions': [],
+            'Null_positions': [],
+        };
+    
+        for (let i = 0; i < 9; i++){
+            if (gameBoard.getField(i) == Player1.getSign()) {
+                usedFields.Player1_positions.push(i);
+                continue;
+            };
+            if (gameBoard.getField(i) == Player2.getSign()) {
+                usedFields.Player2_positions.push(i);
+                continue;
+            };
+            if (gameBoard.getField(i) == null) {
+                usedFields.Null_positions.push(i);
+                continue;
+            };
             
-            for (let i = 0; i < 9; i++){
-                if (gameBoard.getField(i) == Player1.getSign()){
-                    usedFields.Player1_positions.push(i);
-                } else if (gameBoard.getField(i) == Player2.getSign()){
-                    usedFields.Player2_positions.push(i);
-                } else {
-                    usedFields.Null_positions.push(i);
-                }
-            };
+        };
 
-            for (let index = 0; index < winningPositions.length; index++){
-                if (winningPositions[index].every( (item) => usedFields.Player1_positions.includes(item))){
-                    gameWinner = Player1;
-                    alert(`${Player1.getName()} wins!`);
-                    isGameOver = true;
-                } else if (winningPositions[index].every( (item) => usedFields.Player2_positions.includes(item))){
-                    gameWinner = Player2;
-                    alert(`${Player2.getName()} wins!`);
-                    isGameOver = true;
-                } else if ((_players.every((player) => !player.isWinner)) && gameBoard.isFull()){
-                    alert(`It's a draw!`);
-                    isGameOver = true;
-                } else {
-                    isGameOver = false;
-                };
+        return usedFields
+    };
+
+    // Check for winning combinations TK
+    function updateGameStatus() {
+
+        const usedFields = _getBoardPositions()
+
+        const winningPositions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        // actual game outcome logic
+        for (let index = 0; index < winningPositions.length; index++){
+            if (winningPositions[index].every( (item) => usedFields.Player1_positions.includes(item))){
+                _isGameOver = true;
+                _gameWinner = _players[0];
+                _winningCombo = winningPositions[index]
+                break
             };
+            if (winningPositions[index].every( (item) => usedFields.Player2_positions.includes(item))){
+                _isGameOver = true;
+                _gameWinner = _players[1];
+                _winningCombo = winningPositions[index]
+                break
+            };
+            
+        };
+
+        if (gameBoard.isFull() && _gameWinner == null){
+            _isGameOver = true;
+            _winningCombo = null
+        };
     };
 
     const resetGameLogic = () => {
         _currentPlayer = _players[0];
-        isGameOver = false;
-        gameWinner = null;
-    }
-
-    // const runGame = () => {
-    //     while (! isGameOver) {
-    //         // do something, maybe
-    //     }
-    // }
+        _isGameOver = false;
+        _gameWinner = null;
+    };
 
     return {
             getCurrentPlayer,
             switchPlayer,
             makeMove,
-            checkGameStatus,
-            gameWinner,
-            isGameOver,
+            updateGameStatus,
+            getIsGameOver: () => _isGameOver,
+            getWinningCombo: () => _winningCombo,
+            getGameWinner: () => _gameWinner,
             resetGameLogic,
         };
 };
 
-// Testing Game Logic
+
 // Create a couple of players
 const person1 = Player('Player 1', 'X');
 const person2 = Player('Player 2', 'O');
 
 // Game Display IIFE
 const displayController = (() => {
-    // Create a game instance
-    const gameInstance = GameLogic(person1, person2);
+
+    // make game instance
+    gameInstance = GameLogic(person1, person2);
 
     // DOM elements
     const messageSpanDOM = document.getElementById('message-span');
     const resetButtonDOM = document.getElementById('reset-button');
     const gameCells = document.querySelectorAll('.gameboard__cell');
-    
-    // Initial display update
-    updateDisplay();
 
     // changes DOM contents (cells and headings) based off gameboard array
-    function updateDisplay() {
+    function updateBoardDisplay() {
         // update the board
         for (let i = 0; i < gameCells.length; i++){
             gameCells[i].innerHTML = gameBoard.getField(i);
             };
+        };
 
+    function updateMessageDisplay() {
         // update the current player
-        messageSpanDOM.innerHTML = gameInstance.getCurrentPlayer().getName();
+        if (gameInstance.getIsGameOver() == false) {
+            messageSpanDOM.innerHTML = gameInstance.getCurrentPlayer().getName();
+        };
+
+        if (gameInstance.getIsGameOver() == true){
+            let winner = gameInstance.getGameWinner(); 
+
+            if (winner) {
+                messageSpanDOM.innerHTML = `${winner.getName()} wins!`;
+            } else {
+                messageSpanDOM.innerHTML = `It's a tie!`;
+            };
+        };
     };
-    
-    // game cell functionality
-    gameCells.forEach(cell => cell.addEventListener('click', (e) => {
-        const cellIndex = e.target.getAttribute('data-game-cell-index');
-        console.log(`Cell \'${cellIndex}\' clicked by \'${gameInstance.getCurrentPlayer().getName()}`)
-        gameInstance.makeMove(gameInstance.getCurrentPlayer(), cellIndex); //need
-        updateDisplay();
-        gameInstance.checkGameStatus();
-    }));
+
+    function updateAllDisplay() {
+        updateBoardDisplay();
+        updateMessageDisplay();
+    };
 
     // reset button functionality
     resetButtonDOM.addEventListener("click", (e) => {
         gameBoard.resetBoard();
         gameInstance.resetGameLogic();  
-        updateDisplay();
-        console.log('reset button clicked');
+        updateAllDisplay();
     });
 
+    // intitial render
+    updateAllDisplay()
+
+    if (gameInstance.getIsGameOver() == false) {
+        // game cell functionality (depending of if game isGameOver boolean)
+        gameCells.forEach(cell => cell.addEventListener('click', (e) => {
+            const cellIndex = e.target.getAttribute('data-game-cell-index');
+            // e.target.classlist.add(`${gameInstance.getCurrentPlayer().getName}`)
+            // console.log(`Cell \'${cellIndex}\' clicked by \'${gameInstance.getCurrentPlayer().getName()}`)
+            gameInstance.makeMove(gameInstance.getCurrentPlayer(), cellIndex);
+            gameInstance.updateGameStatus();
+            updateAllDisplay();
+            }));
+    };
+    if (gameInstance.getIsGameOver()) {
+        alert("Game has ended. Thanks for playing.")
+    };
 })();
